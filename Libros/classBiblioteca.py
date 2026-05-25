c241eb06ee812115312a267d6d2a767728043289
Facundo <facultrik@gmail.com>
2026-05-25 19:35:07 -0300

    Create classBiblioteca.py

---
+++ Libros/classBiblioteca.py
@@ -0,0 +1,39 @@
+class Biblioteca:
+    def __init__ (self):
+        self.libros = []
+        self.miembros = []
+
+    def agregar_libros(self, libro): 
+        self.libros.append(libro)
+
+    def agregar_miembro(self, miembro):
+        self.miembros.append(miembro)
+
+    def buscar_miembro(self, dni):
+        for m in self.miembros:
+            if m.dni == dni:
+                return m
+        return None
+
+    def buscar_libro(self, titulo):
+        for l in self.libros:
+            if l.titulo.lower() == titulo.lower():
+                return l
+        return None
+
+    def estado_libros(self):
+        print("Estado de libros: ")
+        for libro in self.libros:
+            if libro.disponible:
+                print(f"{libro.titulo} - Disponible")
+            else:
+                print(f"{libro.titulo} - Prestado a {libro.prestadoA.nombre}")
+
+    def estado_miembros(self):
+        print("Estado de miembros")
+        for m in self.miembros:
+            if len(m.libro_prestado) == 0:
+                print(f"{m.nombre} - No tiene libros prestados")
+            else: 
+                for l in m.libro_prestado:
+                    print(f"{m.nombre} - Libros: {l.titulo}")
