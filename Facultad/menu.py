12814962dfad936358d032eb6a47bff4d9640ae5
Facundo <facultrik@gmail.com>
2026-05-25 22:47:09 -0300

    Update menu.py

--- Facultad/menu.py
+++ Facultad/menu.py
@@ -1,10 +1,10 @@
 def mostrar_menu():
 
     print("\n  Gestión de Facultad")
-    print("1. Registrar estudiante")
-    print("2. Registrar curso")
-    print("3. Inscribir estudiante a curso")
-    print("4. Bajar a estudiante de curso")
-    print("5. Mostrar cursos")
-    print("6. Mostrar estudiantes")
-    print("7. Salir")
+    print("1- Registrar estudiante")
+    print("2- Registrar curso")
+    print("3- Inscribir estudiante a curso")
+    print("4- Bajar a estudiante de curso")
+    print("5- Mostrar cursos")
+    print("6- Mostrar estudiantes")
+    print("7- Salir")
